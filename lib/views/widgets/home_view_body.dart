bea99a5b3e44a90ef31584e901e3eed33277f441
waleedelbahnacy <waleedelbahnacy@gmail.com>
2024-05-05 15:18:25 +0300

    add featured list view to home

--- lib/views/widgets/home_view_body.dart
+++ lib/views/widgets/home_view_body.dart
@@ -13,8 +13,27 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children:const [
    CustomAppBar(),
-   CustomListViewItem(),
+   FeaturedBooksListView(),
       ],
+    );
+  }
+}
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+       height: MediaQuery.of(context).size.height *.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context , index){
+        return const Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 8),
+          child:  FeaturedListViewItem(),
+        );
+      }),
     );
   }
 }
