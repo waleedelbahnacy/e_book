6446761c002a87e0a4271937296f8accb6b67591
waleedelbahnacy <waleedelbahnacy@gmail.com>
2024-05-05 16:37:22 +0300

    Best seller list view item part one

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:e_book/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const Bookly());
@@ -18,6 +19,7 @@ class Bookly extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor:KPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
      home: const SplashView()
     );
