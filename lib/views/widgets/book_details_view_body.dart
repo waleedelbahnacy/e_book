29a9f1146c4e8f201f3a044e2483892476a2acee
waleedelbahnacy <waleedelbahnacy@gmail.com>
2024-05-09 23:05:42 +0300

    create books action

--- lib/views/widgets/book_details_view_body.dart
+++ lib/views/widgets/book_details_view_body.dart
@@ -1,6 +1,12 @@
+import 'package:e_book/core/utils/styles.dart';
+import 'package:e_book/core/utils/widgets/custom_button.dart';
+import 'package:e_book/views/widgets/book_rating.dart';
+import 'package:e_book/views/widgets/books_action.dart';
 import 'package:e_book/views/widgets/custom_book_details_app_bar.dart';
 import 'package:e_book/views/widgets/custom_book_irem.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -14,12 +20,42 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
    const   CustomBookDetailsAppBar(),
       Padding(
-        padding: EdgeInsets.symmetric(horizontal: width *.17),
+        padding: EdgeInsets.symmetric(horizontal: width *.2),
         child: CustomBookImage(),
       ),
+      const SizedBox(
+        height:43 ,
+      ),
+      Text('The Jungle Book',
+      style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+      ),
+       const SizedBox(
+        height:6,
+      ),
+      Opacity(
+        opacity: .7,
+        child: Text(
+          'Rudyard Kipling',
+          style: Styles.textStyle18.copyWith(
+            fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      const SizedBox(
+        height :18,
+      ),
+      const BookRating(
+         mainAxisAlignment :MainAxisAlignment.center,
+      ),
+      const SizedBox(
+        height: 37,
+      ),
+   const  BooksAction(),
         ],
       ),
     );
   }
 }
+
 
--- lib/views/widgets/book_rating.dart
+++ lib/views/widgets/book_rating.dart
@@ -1,16 +1,20 @@
 import 'package:e_book/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating({super.key,  this.mainAxisAlignment = MainAxisAlignment.start});
 
+final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         Icon(
           FontAwesomeIcons.solidStar,
+          size: 14,
           color: Color(0xffFFDD4F),
         ),
        const  SizedBox(
@@ -23,11 +27,14 @@ class BookRating extends StatelessWidget {
        const SizedBox(
           width: 5,
         ),
-        Text(
-          '(245)',
-         style: Styles.textStyle14.copyWith(
-          color:const Color(0xff707070,
-          )),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            '(245)',
+           style: Styles.textStyle14.copyWith(
+            fontWeight: FontWeight.w600,
+           ),
+          ),
         ),
       ],
     );
